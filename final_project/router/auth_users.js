384a2b9fbc3ce5de77c31a0a1b576d42b4ad4c59
arya853 <arya.nagaraj2014@gmail.com>
2026-09-02 13:35:34 +0530

    Update auth_users.js

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -53,6 +53,32 @@ regd_users.put("/auth/review/:isbn", (req, res) => {
     reviews: book.reviews
   });
 });
+// Delete a book review
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+  const isbn = req.params.isbn;
+  const { username } = req.body;
+
+  const book = books[isbn];
+
+  if (!book) {
+    return res.status(404).json({
+      message: "Book not found"
+    });
+  }
+
+  if (!book.reviews[username]) {
+    return res.status(404).json({
+      message: "Review not found"
+    });
+  }
+
+  delete book.reviews[username];
+
+  return res.status(200).json({
+    message: "Review deleted successfully",
+    reviews: book.reviews
+  });
+});
 
 module.exports.authenticated = regd_users;
 module.exports.isValid = isValid;
